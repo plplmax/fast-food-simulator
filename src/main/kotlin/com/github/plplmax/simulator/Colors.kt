c8b6fe1920fca6d2a20de7eac21edc7c1af1da14
plplmax <50287455+plplmax@users.noreply.github.com>
2022-09-25 19:36:43 +0300

    feat: improve UI

---
+++ src/main/kotlin/com/github/plplmax/simulator/Colors.kt
@@ -0,0 +1,6 @@
+package com.github.plplmax.simulator
+
+import androidx.compose.material.Colors
+import androidx.compose.material.darkColors
+
+val colors: Colors = darkColors()
--- src/main/kotlin/com/github/plplmax/simulator/Main.kt
+++ src/main/kotlin/com/github/plplmax/simulator/Main.kt
@@ -1,26 +1,42 @@
 package com.github.plplmax.simulator
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Placeable
+import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.layout.SubcomposeMeasureScope
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
+import com.github.plplmax.simulator.order.OrderState
 import com.github.plplmax.simulator.order.OrderStateOf
 import com.github.plplmax.simulator.restaurant.RestaurantOf
 
 fun main() = singleWindowApplication {
-    MaterialTheme {
-        Surface(modifier = Modifier.fillMaxSize(), color = Color.DarkGray, shape = MaterialTheme.shapes.large) {
+    MaterialTheme(colors) {
+        Box(
+            modifier = Modifier.background(MaterialTheme.colors.surface)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
             MainScreen()
         }
     }
@@ -30,7 +46,11 @@ fun main() = singleWindowApplication {
 private fun MainScreen() {
     val orderState = remember { OrderStateOf() }
     val restaurant = remember { RestaurantOf(orderState) }
-    OrderArea(orderState.customersSize, orderState.currentOrderId)
+    Column(modifier = Modifier.heightIn(max = 260.dp)) {
+        Row {
+            OrderArea(state = orderState)
+        }
+    }
     LaunchedEffect(Unit) {
         restaurant.start(scope = this@LaunchedEffect)
         restaurant.makeOrdersEndlessly()
@@ -38,27 +58,103 @@ private fun MainScreen() {
 }
 
 @Composable
-private fun OrderArea(waitingCustomers: Int = 0, currentOrderId: Int = 0) {
-    Row {
-        OrderText("Wait to place orders: $waitingCustomers")
-        CurrentOrderText(if (currentOrderId == 0) "empty" else currentOrderId.toString())
+private fun OrderArea(state: OrderState = OrderStateOf()) {
+    SubcomposeFlexColumn {
+        InformationCard {
+            TextCardContainer {
+                Text("Wait to place orders: ")
+                Text("${state.customersSize}")
+            }
+        }
+        InformationCard {
+            CurrentOrderText(state.currentOrderId)
+        }
+    }
+}
+
+@Composable
+private fun SubcomposeFlexColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    SubcomposeLayout(modifier) { constraints ->
+        var slotId = 0
+        val placeables = subcompose(slotId++, content).map { measurable ->
+            measurable.measure(constraints)
+        }
+        val maxSize = placeables.fold(initial = IntSize.Zero) { max, placeable ->
+            IntSize(width = maxOf(max.width, placeable.width), height = max.height + placeable.height)
+        }
+        val trmdPlaceables = trimmedPlaceables(maxSize, placeables, constraints, slotId, content)
+        layout(width = maxSize.width, height = constraints.maxHeight) {
+            var yPos = 0
+            trmdPlaceables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPos)
+                yPos += placeable.height
+            }
+        }
+    }
+}
+
+private fun SubcomposeMeasureScope.trimmedPlaceables(
+    maxSize: IntSize,
+    placeables: List<Placeable>,
+    constraints: Constraints,
+    slotId: Int,
+    content: @Composable () -> Unit
+): Sequence<Placeable> {
+    var currentHeight = maxSize.height
+    val trimmedPlaceables = placeables.dropLastWhile {
+        if (currentHeight > constraints.maxHeight) {
+            currentHeight -= it.height
+            true
+        } else {
+            false
+        }
+    }
+    if (trimmedPlaceables.isEmpty()) return emptySequence()
+    val height = constraints.maxHeight / trimmedPlaceables.size
+    return subcompose(slotId, content).asSequence().map { measurable ->
+        measurable.measure(
+            Constraints(
+                minWidth = maxSize.width,
+                maxWidth = constraints.maxWidth,
+                minHeight = height,
+                maxHeight = height
+            )
+        )
+    }.take(trimmedPlaceables.size)
+}
+
+@Composable
+private fun InformationCard(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    BoxWithConstraints(contentAlignment = Alignment.Center) {
+        Card(modifier = modifier.widthIn(min = minWidth), elevation = 4.dp) {
+            content()
+        }
+    }
+}
+
+@Composable
+private fun TextCardContainer(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Row(modifier = modifier.padding(24.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+        content()
     }
 }
 
 @Preview
 @Composable
 private fun OrderAreaPreview() {
-    MaterialTheme {
+    MaterialTheme(colors) {
         OrderArea()
     }
 }
 
 @Composable
-private fun OrderText(text: String) {
-    Surface(border = BorderStroke(4.dp, Color.Black)) {
-        Text(text, modifier = Modifier.padding(24.dp))
+private fun CurrentOrderText(currentOrderId: Int) {
+    val text = if (currentOrderId == 0) "N/A" else currentOrderId.toString()
+    TextCardContainer {
+        Text("Number of current order: ")
+        Text(text, modifier = Modifier.padding(start = 14.dp))
     }
 }
-
-@Composable
-private fun CurrentOrderText(text: String): Unit = OrderText("Number of current order: $text")
