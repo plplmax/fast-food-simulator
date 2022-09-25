2b63f4023cc6f29eee92141bb9bfa1f4bd570587
plplmax <50287455+plplmax@users.noreply.github.com>
2022-09-26 01:10:06 +0300

    feat: create `AppColors`

--- src/main/kotlin/com/github/plplmax/simulator/Colors.kt
+++ src/main/kotlin/com/github/plplmax/simulator/Colors.kt
@@ -2,5 +2,11 @@ package com.github.plplmax.simulator
 
 import androidx.compose.material.Colors
 import androidx.compose.material.darkColors
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val gray12: Color = Color(0xFF1F1F1F)
+    val gray16: Color = Color(0xFF292929)
+}
 
 val colors: Colors = darkColors()
